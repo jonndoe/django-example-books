3e4996aa62ccb8870c5586c2b1c1dfcee31ad46f
jonndoe <lukichevvasily@gmail.com>
2021-06-25 11:21:40 +0100

    added book model

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     "example.users.apps.UsersConfig",
+    "example.books.apps.BooksConfig",
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
