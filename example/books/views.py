41f3152353ce9aa5fa4a978d0e9049fb8e126d1d
jonndoe <lukichevvasily@gmail.com>
2021-06-25 12:52:51 +0100

    added book_detail url, view, template

--- example/books/views.py
+++ example/books/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # use this import instead of short 'from .models' to escape RuntimeErros
 # https://medium.com/@michal.bock/fix-weird-exceptions-when-running-django-tests-f58def71b59a
@@ -9,3 +9,10 @@ class BookListView(ListView):
     model = Book
     context_object_name = "book_list"
     template_name = "books/book_list.html"
+
+
+class BookDetailView(DetailView):
+    model = Book
+    context_object_name = 'book'
+    template_name = 'books/book_detail.html'
+
